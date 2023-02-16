85a12453e8bfb5cf6bac3b0edd5a9afef8a6e496
glumslug <glumaslug@gmail.com>
2023-02-16 01:18:03 -0500

    added back in some body parser middleware

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,8 @@ app.use(cors());
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+app.use(bodyParser.text({ type: "text/html" }));
 
 //Routes
 app.get("/", (req, res) => {
